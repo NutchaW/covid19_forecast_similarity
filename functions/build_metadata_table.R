22049c80e6f6ce5fd41bd2346c64d7a18f1ee971
Nutcha Wattanachit <nutcha.wattanachit@gmail.com>
2021-06-10 08:55:07 -0400

    update metadata functions

--- functions/build_metadata_table.R
+++ functions/build_metadata_table.R
@@ -9,12 +9,13 @@ source("./functions/metadata_functions.R")
 source("./functions/distance_func_script.R")
 
 # make full list
-full_list <- list.files("./data/metadata")
-full_list_death <- map_dfr(full_list, 
-                           function(x) {
-                             make_metadata_table(paste0("./data/metadata/",x))
-                           })
-
+local_path <- "/Users/ahctun_woon/git/covid19-forecast-hub/"
+model_list <- basename(list.dirs(paste0(local_path,
+                                        "data-processed/"), recursive=FALSE))
+full_list <- map_dfr(model_list, 
+                     function(x) {
+                       get_model_metadata(x, local_path)
+                       })
 # extract
-categorized_frame <- extract_data(full_list_death)
+categorized_frame <- make_metadata_table(full_list,summarizing = TRUE)
 write.csv(categorized_frame,"./metadata_categorized.csv",row.names = FALSE)
