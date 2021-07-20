8bb5b768e6d12d1ed34031fb5b2dd05e0189f24e
lshandross <57642277+lshandross@users.noreply.github.com>
2021-07-20 14:03:17 -0400

    build sat data

--- functions/build_data_hosp.R
+++ functions/build_data_hosp.R
@@ -4,26 +4,24 @@ library(zoltr)
 library(covidHubUtils)
 
 # script to build a dataframe of model 
-# first sat end date for 1 wk ahead
-first_end_date <- as.Date("2021-01-28") # change this to January some time after new year
-
-most_recent_end_date <- as.Date("2021-06-10") # pick a date
-  
-# use lubridate- or maybe seq () to get a range of dates between the first end date and the most recent one
-date_range <- seq.Date(from = first_end_date, to = most_recent_end_date, by = "week")
-date_range <- as.Date(date_range)
 
 # set targets for analysis
 target_list <- paste0(1:28," day ahead inc hosp")
 
+## Thursdays only ---------------------------------------------------- ##########################
+first_end_date_thurs <- as.Date("2021-01-28") 
+most_recent_end_date_thurs <- as.Date("2021-06-10")
+  
+date_range_thurs <- seq.Date(from = first_end_date_thurs, 
+                             to = most_recent_end_date_thurs, by = "week")
 
 # get locations
-hosp_truth <- load_truth("HealthData", #note this is the only source available
+hosp_truth_thurs <- load_truth("HealthData", 
                          "inc hosp", 
                          temporal_resolution="weekly",
                          data_location = "remote_hub_repo") %>%
-  dplyr::filter(target_end_date >= first_end_date,
-                target_end_date <= most_recent_end_date,
+  dplyr::filter(target_end_date >= first_end_date_thurs,
+                target_end_date <= most_recent_end_date_thurs,
                 geo_type=="state",
                 location != "US") %>%
   dplyr::group_by(location) %>%
@@ -34,24 +32,24 @@ hosp_truth <- load_truth("HealthData", #note this is the only source available
   dplyr::distinct() %>%
   dplyr::arrange(desc(cum_hosp))
 
-hlocs_high <- hosp_truth$location[1:5]
+hlocs_high_thurs <- hosp_truth_thurs$location[1:5]
 
 
 # run to build
-hosp_forecasts_high <- map_dfr(date_range,
+hosp_forecasts_high_thurs <- map_dfr(date_range_thurs,
                  function(x) {
                    covidHubUtils::load_latest_forecasts(last_forecast_date = x,
                                                         forecast_date_window_size=6,
                                                         # pick one
-                                                        locations = hlocs_high,
+                                                        locations = hlocs_high_thurs,
                                                         types = "quantile",
                                                         targets = target_list,
                                                         source = "zoltar")
                    })
 
 #create new col to make division of forecasts - by "week" (7 days of forecasts grouped together)
-hosp_forecasts_high <- hosp_forecasts_high %>%
-  dplyr::filter(weekdays(`target_end_date`) == weekdays(most_recent_end_date)) %>%
+hosp_forecasts_high_thurs <- hosp_forecasts_high_thurs %>%
+  dplyr::filter(weekdays(`target_end_date`) == weekdays(most_recent_end_date_thurs)) %>%
   dplyr::mutate(horizon_week = case_when(
     horizon %in% 1:7 ~ 1,
     horizon %in% 8:14 ~ 2, 
@@ -60,18 +58,18 @@ hosp_forecasts_high <- hosp_forecasts_high %>%
   ))
 
 # write large files
-write_csv(hosp_forecasts_high,file = "./data/quantile_frame_hosp_top.csv")
+write_csv(hosp_forecasts_high_thurs,file = "./data/quantile_frame_hosp_top.csv")
 
 
-## --------- Low -------------------------------------------------##
-hosp_truth_low <- hosp_truth %>%
+## --------- Low --------------------------------------------------------------------------------##
+hosp_truth_thurs_low <- hosp_truth_thurs %>%
   dplyr::arrange(cum_hosp) %>%
   dplyr::filter(as.numeric(location)<60)
 
-hlocs_low <- hosp_truth_low$location[1:5]
+hlocs_low <- hosp_truth_thurs_low$location[1:5]
 
 # run to build
-hosp_forecasts_low <- map_dfr(date_range,
+hosp_forecasts_low <- map_dfr(hosp_forecasts_high_thurs,
                                function(x) {
                                  covidHubUtils::load_latest_forecasts(
                                    last_forecast_date = x,
@@ -83,30 +81,28 @@ hosp_forecasts_low <- map_dfr(date_range,
                                    source = "zoltar")
                                })
 
-
 #create new col to make division of forecasts - by "week" (7 days of forecasts grouped together)
 hosp_forecasts_low <- hosp_forecasts_low %>%
-  dplyr::filter(weekdays(`target_end_date`) == weekdays(most_recent_end_date)) %>%
+  dplyr::filter(weekdays(`target_end_date`) == weekdays(most_recent_end_date_thurs)) %>%
   dplyr::mutate(horizon_week = case_when(
     horizon %in% 1:7 ~ 1,
     horizon %in% 8:14 ~ 2, 
     horizon %in% 15:21 ~ 3,
     horizon %in% 21:28 ~ 4,
   ))
-                  
 
 # write large files
 write_csv(hosp_forecasts_low,file = "./data/quantile_frame_hosp_bottom.csv")
 
 
 
-###### Point Forecasts ---------------------------------------------------- ##########################
-hosp_pt_high <- map_dfr(date_range,
+###### Point Forecasts -------------------------------------------------------------------------##
+hosp_pt_high <- map_dfr(hosp_forecasts_high_thurs,
                         function(x) {
                           covidHubUtils::load_latest_forecasts(last_forecast_date = x,
                                                                forecast_date_window_size=6,
                                                                # pick one
-                                                               locations = hlocs_high,
+                                                               locations = hlocs_high_thurs,
                                                                types = "point",
                                                                targets = target_list,
                                                                source = "zoltar")
@@ -125,7 +121,7 @@ hosp_pt_high <- hosp_pt_high %>%
 write_csv(hosp_pt_high,file = "./data/point_frame_hosp_top.csv")
 
 ##
-hosp_pt_low <- map_dfr(date_range,
+hosp_pt_low <- map_dfr(hosp_forecasts_high_thurs,
                         function(x) {
                           covidHubUtils::load_latest_forecasts(last_forecast_date = x,
                                                                forecast_date_window_size=6,
@@ -147,3 +143,129 @@ hosp_pt_low <- hosp_pt_low %>%
 
 # write large files
 write_csv(hosp_pt_low,file = "./data/point_frame_hosp_bottom.csv")
+
+
+
+
+## Saturdays only ---------------------------------------------------- ##########################
+first_end_date_sat <- as.Date("2021-01-30") 
+most_recent_end_date_sat <- as.Date("2021-06-12")
+
+# note that hlocs_high_sat are different, but we use hlocs_high_thurs for consistency
+# hlocs_high_sat = Florida, Texas, New York, California, Michigan
+# PA replaces MI for thurs locs, but it is only by ~200 hospitalizations
+
+date_range_sat <- seq.Date(from = first_end_date_sat, 
+                             to = most_recent_end_date_sat, by = "week")
+
+
+# run to build
+hosp_forecasts_high_sat <- map_dfr(date_range_sat,
+                                     function(x) {
+                                       covidHubUtils::load_latest_forecasts(last_forecast_date = x,
+                                                                            forecast_date_window_size=6,
+                                                                            locations = hlocs_high_thurs,
+                                                                            types = "quantile",
+                                                                            targets = target_list,
+                                                                            source = "zoltar")
+                                     })
+
+#create new col to make division of forecasts - by "week" (7 days of forecasts grouped together)
+hosp_forecasts_high_sat <- hosp_forecasts_high_sat %>%
+  dplyr::filter(weekdays(`target_end_date`) == weekdays(most_recent_end_date_sat)) %>%
+  dplyr::mutate(horizon_week = case_when(
+    horizon %in% 1:7 ~ 1,
+    horizon %in% 8:14 ~ 2, 
+    horizon %in% 15:21 ~ 3,
+    horizon %in% 21:28 ~ 4,
+  ))
+
+# write large files
+write_csv(hosp_forecasts_high_sat,file = "./data/quantile_frame_hosp_top_sat.csv")
+
+
+## --------- Low --------------------------------------------------------------------------------##
+hosp_truth_sat_low <- hosp_truth_sat %>%
+  dplyr::arrange(cum_hosp) %>%
+  dplyr::filter(as.numeric(location)<60)
+
+
+hlocs_low <- hosp_truth_sat_low$location[1:5]
+# note that hlocs_low_sat is the same as hlocs_low_thurs
+
+# run to build
+hosp_forecasts_low_sat <- map_dfr(date_range_sat,
+                              function(x) {
+                                covidHubUtils::load_latest_forecasts(
+                                  last_forecast_date = x,
+                                  forecast_date_window_size=6,
+                                  # pick one
+                                  locations = hlocs_low_thurs,
+                                  types = "quantile",
+                                  targets = target_list,
+                                  source = "zoltar")
+                              })
+
+#create new col to make division of forecasts - by "week" (7 days of forecasts grouped together)
+hosp_forecasts_low <- hosp_forecasts_low %>%
+  dplyr::filter(weekdays(`target_end_date`) == weekdays(most_recent_end_date_sat)) %>%
+  dplyr::mutate(horizon_week = case_when(
+    horizon %in% 1:7 ~ 1,
+    horizon %in% 8:14 ~ 2, 
+    horizon %in% 15:21 ~ 3,
+    horizon %in% 21:28 ~ 4,
+  ))
+
+# write large files
+write_csv(hosp_forecasts_low,file = "./data/quantile_frame_hosp_bottom_sat.csv")
+
+
+
+###### Point Forecasts -------------------------------------------------------------------------##
+hosp_pt_high_sat <- map_dfr(date_range_sat,
+                        function(x) {
+                          covidHubUtils::load_latest_forecasts(last_forecast_date = x,
+                                                               forecast_date_window_size=6,
+                                                               # pick one
+                                                               locations = hlocs_high_thurs,
+                                                               types = "point",
+                                                               targets = target_list,
+                                                               source = "zoltar")
+                        })
+
+#create new col to make division of forecasts - by "week" (7 days of forecasts grouped together)
+hosp_pt_high_sat <- hosp_pt_high_sat %>% 
+  dplyr::mutate(horizon_week = case_when(
+    horizon %in% 1:7 ~ 1,
+    horizon %in% 8:14 ~ 2, 
+    horizon %in% 15:21 ~ 3,
+    horizon %in% 21:28 ~ 4,
+  )) 
+
+# write large files
+write_csv(hosp_pt_high_sat,file = "./data/point_frame_hosp_top_sat.csv")
+
+##
+hosp_pt_low_sat <- map_dfr(date_range_sat,
+                       function(x) {
+                         covidHubUtils::load_latest_forecasts(last_forecast_date = x,
+                                                              forecast_date_window_size=6,
+                                                              # pick one
+                                                              locations = hlocs_low,
+                                                              types = "point",
+                                                              targets = target_list,
+                                                              source = "zoltar")
+                       })
+
+#create new col to make division of forecasts - by "week" (7 days of forecasts grouped together)
+hosp_pt_low_sat <- hosp_pt_low_sat %>% 
+  dplyr::mutate(horizon_week = case_when(
+    horizon %in% 1:7 ~ 1,
+    horizon %in% 8:14 ~ 2, 
+    horizon %in% 15:21 ~ 3,
+    horizon %in% 21:28 ~ 4,
+  )) 
+
+# write large files
+write_csv(hosp_pt_low_sat,file = "./data/point_frame_hosp_bottom_sat.csv")
+
